3eedc1c32900bf2fe0ecd9c8797ae13515521899
justin-pyne <jpyne.justin@gmail.com>
2024-10-09 13:47:34 -0700

    implement controller, webhooks and postman testing

--- src/main/java/usfca/pyne/cs601/virtualfridge/CommandLineAppStartupRunner.java
+++ src/main/java/usfca/pyne/cs601/virtualfridge/CommandLineAppStartupRunner.java
@@ -1,22 +1,22 @@
-//package usfca.pyne.cs601.virtualfridge;
-//
-//import java.util.List;
-//import usfca.pyne.cs601.virtualfridge.Model.Recipe;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//import usfca.pyne.cs601.virtualfridge.Service.RecipeService;
-//
-//@Component
-//public class CommandLineAppStartupRunner implements CommandLineRunner {
-//
-//    private final RecipeService recipeService;
-//
-//    public CommandLineAppStartupRunner(RecipeService recipeService) {
-//        this.recipeService = recipeService;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
+package usfca.pyne.cs601.virtualfridge;
+
+import java.util.List;
+import usfca.pyne.cs601.virtualfridge.Model.Recipe;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import usfca.pyne.cs601.virtualfridge.Service.RecipeService;
+
+@Component
+public class CommandLineAppStartupRunner implements CommandLineRunner {
+
+    private final RecipeService recipeService;
+
+    public CommandLineAppStartupRunner(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
 //        String ingredients = "1 lb Ground beef, 2 onions, 2 tomatoes, 1 can of tomato paste, chicken stock, carrots, sweet potato, corn, peas, cilantro, lime, cheese";
 //        String response = recipeService.generateRecipe(ingredients);
 //        System.out.println("Raw response: ");
@@ -29,5 +29,5 @@
 //        } else {
 //            System.err.println("No recipes were generated.");
 //        }
-//    }
-//}
+    }
+}
