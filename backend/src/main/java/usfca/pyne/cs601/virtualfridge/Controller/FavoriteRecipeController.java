6c00f313cea55b71c613351a137d7d0f379d7f52
justin-pyne <jpyne.justin@gmail.com>
2024-10-10 18:11:44 -0700

    Adding IngredientController and FavoriteRecipeController

--- backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/FavoriteRecipeController.java
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/FavoriteRecipeController.java
@@ -1,4 +1,50 @@
 package usfca.pyne.cs601.virtualfridge.Controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import usfca.pyne.cs601.virtualfridge.Model.FavoriteRecipe;
+import usfca.pyne.cs601.virtualfridge.Model.Recipe;
+import usfca.pyne.cs601.virtualfridge.Service.FavoriteRecipeService;
+import usfca.pyne.cs601.virtualfridge.Service.RecipeService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/favorites")
 public class FavoriteRecipeController {
+
+    private final FavoriteRecipeService favoriteRecipeService;
+    private final RecipeService recipeService;
+
+    public FavoriteRecipeController(FavoriteRecipeService favoriteRecipeService, RecipeService recipeService) {
+        this.favoriteRecipeService = favoriteRecipeService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Recipe>> getAllFavoriteRecipes() {
+        List<FavoriteRecipe> favoriteRecipes = favoriteRecipeService.getAllFavoriteRecipes();
+        List<Recipe> recipes = favoriteRecipes.stream()
+                .map(FavoriteRecipe::getRecipe)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(recipes);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addFavoriteRecipe(@RequestParam Long recipeId) {
+        boolean isFavorited = favoriteRecipeService.isRecipeFavorited(recipeId);
+        if(isFavorited) {
+            return ResponseEntity.badRequest().body("Recipe is already favorited.");
+        }
+        favoriteRecipeService.addFavoriteRecipe(recipeId);
+        return ResponseEntity.ok("Recipe added to favorites.");
+    }
+
+    @DeleteMapping("/{recipeId}")
+    public ResponseEntity<String> removeFavoriteRecipe(@PathVariable Long recipeId) {
+        favoriteRecipeService.removeFavoriteRecipe(recipeId);
+        return ResponseEntity.ok("Recipe removed from favorites.");
+    }
 }
--- backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/IngredientController.java
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/IngredientController.java
@@ -1,4 +1,37 @@
 package usfca.pyne.cs601.virtualfridge.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import usfca.pyne.cs601.virtualfridge.Model.Ingredient;
+import usfca.pyne.cs601.virtualfridge.Service.IngredientService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fridge")
 public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @GetMapping("/ingredients")
+    public ResponseEntity<List<Ingredient>> getAllIngredients(){
+        List<Ingredient> ingredients = ingredientService.getAllIngredients();
+        return ResponseEntity.ok(ingredients);
+    }
+
+    @PostMapping("/ingredients")
+    public ResponseEntity<Ingredient> addOrUpdateIngredient(@RequestBody Ingredient ingredient){
+        Ingredient addedIngredient = ingredientService.addOrUpdateIngredient(ingredient);
+        return ResponseEntity.ok(addedIngredient);
+    }
+
+    @DeleteMapping("/ingredients/{id}")
+    public ResponseEntity<Void> deleteIngredient(@PathVariable Long id) {
+        ingredientService.removeIngredient(id);
+        return ResponseEntity.noContent().build();
+    }
 }
