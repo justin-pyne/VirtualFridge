3aaa429b81bdf208fd2e5f2f18e3525cc6794137
justin-pyne <jpyne.justin@gmail.com>
2024-10-10 18:12:40 -0700

    Adding cookRecipe functionality

--- backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/RecipeController.java
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/RecipeController.java
@@ -1,29 +1,37 @@
 package usfca.pyne.cs601.virtualfridge.Controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import usfca.pyne.cs601.virtualfridge.Model.Recipe;
 import usfca.pyne.cs601.virtualfridge.Service.RecipeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/fridge")
+@RequestMapping("/recipes")
 public class RecipeController {
+
+    private RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService){
         this.recipeService = recipeService;
     }
 
-    private RecipeService recipeService;
 
-    @PostMapping("/ingredients")
-    public ResponseEntity<List<Recipe>> getRecipes(@RequestBody String ingredients){
-        List<Recipe> recipes = recipeService.generateRecipe(ingredients);
-        return ResponseEntity.ok().body(recipes);
+    @GetMapping
+    public ResponseEntity<List<Recipe>> getGeneratedRecipes(){
+        List<Recipe> recipes = recipeService.generateRecipe();
+        return ResponseEntity.ok(recipes);
     }
 
+    @PostMapping("/cook/{recipeId}")
+    public ResponseEntity<String> cookRecipe(@PathVariable Long recipeId) {
+        boolean success = recipeService.cookRecipe(recipeId);
+        if(success){
+            return ResponseEntity.ok("Recipe cooked successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Insufficient ingredients for this recipe.");
+        }
+    }
 }
