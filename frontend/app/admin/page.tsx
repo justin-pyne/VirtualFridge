6fc2d1361ead5a3a534f946fe886f1dfc3a42f57
gursh <95269077+dzyfer@users.noreply.github.com>
2024-11-03 17:25:37 -0800

    Add UserTable to Admin page

---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import UsersTable from "../components/UsersTable";
+
+export default function AdminPage() {
+    return (
+        <div>
+            <UsersTable />
+        </div>
+    )
+};
