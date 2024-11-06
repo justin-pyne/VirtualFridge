7d72edb85646790c3f03742f041149b1a70f8fc0
justin-pyne <jpyne.justin@gmail.com>
2024-11-06 15:06:17 -0800

    Adding Fridge logic to UserEntity

--- backend/src/main/java/usfca/pyne/cs601/virtualfridge/Entity/UserEntity.java
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Entity/UserEntity.java
@@ -3,11 +3,15 @@ package usfca.pyne.cs601.virtualfridge.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import usfca.pyne.cs601.virtualfridge.Model.FavoriteRecipe;
+import usfca.pyne.cs601.virtualfridge.Model.Fridge;
+
+import java.util.List;
 
 @Setter
 @Getter
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = {"email"}))
 public class UserEntity {
 
     @Id
@@ -16,13 +20,23 @@ public class UserEntity {
     private String username;
     private String firstName;
     private String lastName;
+
+    @Column(nullable = false)
     private String email;
 
-    public UserEntity(long id, String username, String firstName, String lastName) {
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Fridge fridge;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<FavoriteRecipe> favoriteRecipes;
+
+
+    public UserEntity(long id, String username, String firstName, String lastName, String email) {
         this.id = id;
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
     }
 
     public UserEntity() {
