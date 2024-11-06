d2730ec12c3b819ebe8227566909185f287d0455
justin-pyne <jpyne.justin@gmail.com>
2024-11-06 15:52:09 -0800

    findByEmail in UserRepository

--- backend/src/main/java/usfca/pyne/cs601/virtualfridge/Repository/UserRepository.java
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Repository/UserRepository.java
@@ -3,8 +3,9 @@ package usfca.pyne.cs601.virtualfridge.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import usfca.pyne.cs601.virtualfridge.Entity.UserEntity;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+    Optional<UserEntity> findByEmail(String email);
 }
