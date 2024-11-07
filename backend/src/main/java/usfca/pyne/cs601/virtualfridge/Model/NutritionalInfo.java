6bb034a1794b60901e7570f53e05c73da341da6d
justin-pyne <jpyne.justin@gmail.com>
2024-11-07 14:55:40 -0800

    NutritionalInfo class for OpenFoodFacts API

---
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Model/NutritionalInfo.java
@@ -0,0 +1,13 @@
+package usfca.pyne.cs601.virtualfridge.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NutritionalInfo {
+    private Double calories;
+    private Double protein;
+    private Double carbs;
+    private Double fat;
+}
