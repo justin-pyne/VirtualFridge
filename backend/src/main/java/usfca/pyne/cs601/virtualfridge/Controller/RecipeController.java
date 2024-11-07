0f487ddde851e3c61ed19a2ad58b450fc476435c
justin-pyne <jpyne.justin@gmail.com>
2024-11-06 17:36:57 -0800

    Added User logic to RecipeController

--- backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/RecipeController.java
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Controller/RecipeController.java
@@ -22,16 +22,21 @@ public class RecipeController {
 
     @NotNull
     @GetMapping("/get")
-    public ResponseEntity<List<Recipe>> getGeneratedRecipes(){
+    public ResponseEntity<List<Recipe>> getGeneratedRecipes(@RequestParam("email") String email) {
         ResponseEntity.status(HttpStatus.OK).body("Generating new recipes");
-        List<Recipe> recipes = recipeService.generateRecipe();
-        return ResponseEntity.ok(recipes);
+        List<Recipe> recipes = recipeService.generateRecipe(email);
+        if (recipes != null && !recipes.isEmpty()) {
+            return ResponseEntity.ok(recipes);
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(recipes);
+        }
     }
 
     @NotNull
     @PostMapping("/cook/{recipeId}")
-    public ResponseEntity<String> cookRecipe(@PathVariable Long recipeId) {
-        boolean success = recipeService.cookRecipe(recipeId);
+    public ResponseEntity<String> cookRecipe(@RequestParam("email") String email,
+                                             @PathVariable Long recipeId) {
+        boolean success = recipeService.cookRecipe(email, recipeId);
         if(success){
             return ResponseEntity.ok("Recipe cooked successfully.");
         } else {
