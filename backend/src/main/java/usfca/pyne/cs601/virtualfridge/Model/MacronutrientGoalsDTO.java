a0ad86e5f0db24bb1c2a0d6b08c583a18e42aab7
justin-pyne <jpyne.justin@gmail.com>
2024-11-07 14:53:26 -0800

    DTO for macro goals

---
+++ backend/src/main/java/usfca/pyne/cs601/virtualfridge/Model/MacronutrientGoalsDTO.java
@@ -0,0 +1,13 @@
+package usfca.pyne.cs601.virtualfridge.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MacronutrientGoalsDTO {
+    private Double dailyCalorieGoal;
+    private Double dailyProteinGoal;
+    private Double dailyCarbGoal;
+    private Double dailyFatGoal;
+}
