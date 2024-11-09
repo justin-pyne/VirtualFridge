e072d85fa0aa3364c9a5d556097c3543abb3321b
dzyfer <95269077+dzyfer@users.noreply.github.com>
2024-11-08 23:46:56 -0800

    Implement UserTableComponent

---
+++ frontend/components/UserTableComponent/UserTableComponent.tsx
@@ -0,0 +1,75 @@
+import { MoreHorizontal, Pencil, Trash } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { User } from "@/components/UsersTable/UsersTable"
+
+interface UserTableProps {
+  users: User[]
+  onEdit: (user: User) => void
+  onDelete: (user: User) => void
+}
+
+export function UserTableComponent({ users, onEdit, onDelete }: UserTableProps) {
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="font-medium">FIRST NAME</TableHead>
+            <TableHead className="font-medium">LAST NAME</TableHead>
+            <TableHead className="font-medium">EMAILID</TableHead>
+            <TableHead className="text-right font-medium">ACTIONS</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.firstName}</TableCell>
+              <TableCell>{user.lastName}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell className="text-right">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                      <span className="sr-only">Open menu</span>
+                      <MoreHorizontal className="h-4 w-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem
+                      onClick={() => onEdit(user)}
+                      className="text-primary cursor-pointer gap-2"
+                    >
+                      <Pencil className="h-4 w-4" />
+                      Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      onClick={() => onDelete(user)}
+                      className="text-red-600 cursor-pointer gap-2"
+                    >
+                      <Trash className="h-4 w-4" />
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
